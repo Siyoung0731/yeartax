f23ea41f27d999ea41092fa6a1e0347d40ef2894
Siyoung0731 <azxx8731@gmail.com>
2026-06-25 00:10:15 +0900

    first commit

---
+++ src/main/java/com/example/fintax/YeartaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeartaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeartaxApplication.class, args);
+	}
+
+}
